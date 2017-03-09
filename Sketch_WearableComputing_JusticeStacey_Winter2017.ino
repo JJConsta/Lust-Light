dda36a3a978d114efec4cfee4ef026cc007d12e8
JJConsta <jjstacey13@gmail.com>
2017-03-09 15:29:53 -0500

    Add files via upload

---
+++ Sketch_WearableComputing_JusticeStacey_Winter2017.ino
@@ -0,0 +1,22 @@
+
+int LEDred = 13; 
+int LEDwhite = 11; 
+int analogPin = A0; 
+
+void setup() {
+  pinMode (LEDred, OUTPUT);
+  pinMode (LEDwhite, OUTPUT);
+  pinMode (analogPin, INPUT); 
+  Serial.begin (9600);
+}
+
+void loop() {
+ Serial.println(analogRead(0));
+ if (analogRead(A0) < 1023 ) {
+digitalWrite(13, HIGH);
+digitalWrite(11, LOW);
+} else {
+digitalWrite(13, LOW);
+digitalWrite(11, HIGH);
+}
+}
